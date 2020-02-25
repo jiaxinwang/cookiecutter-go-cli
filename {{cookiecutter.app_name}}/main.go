922fc795d7f756652af5efb5cf2724b49a0fdb25
jiaxinwang <jiaxinwang@me.com>
2020-02-25 16:47:55 +0800

    + cli

--- {{cookiecutter.app_name}}/main.go
+++ {{cookiecutter.app_name}}/main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"pt/action"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	logrus.Print("Hello.")
+	logrus.SetFormatter(&nested.Formatter{
+		TimestampFormat: "01/02/15:04:05",
+		HideKeys:        false,
+		ShowFullLevel:   true,
+		FieldsOrder:     []string{"component", "category"},
+	})
+	logrus.SetReportCaller(true)
+	logrus.SetLevel(logrus.TraceLevel)
+	logFile, err := os.OpenFile("logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(fmt.Sprintf("[Error]: %s", err))
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	logrus.SetOutput(mw)
+
+	app := cli.NewApp()
+	app.Name = "{{cookiecutter.app_name}}"
+	app.Version = "0.1.0"
+	app.Commands = []*cli.Command{
+		&action.Echo,
+	}
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "star, s",
+			Value: "the stars look very different today",
+		},
+	}
+
+	err = app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
 }
