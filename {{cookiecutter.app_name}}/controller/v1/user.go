525a8705f3a3e86eb0f80be61327ac5480dd6254
jiaxinwang <jiaxinwang@me.com>
2020-06-01 17:19:58 +0800

    + jwt

--- {{cookiecutter.app_name}}/controller/v1/user.go
+++ {{cookiecutter.app_name}}/controller/v1/user.go
@@ -40,29 +40,3 @@ func Signup(c *gin.Context) {
 	userState.AddUser(param.Name, param.Password, param.Email)
 	response.Response(c, 0, "", nil)
 }
-
-// Login ...
-func Login(c *gin.Context) {
-	requestID := c.MustGet("requestID")
-	var param struct {
-		Username string
-		Password string
-	}
-
-	if err := c.ShouldBindJSON(&param); err != nil {
-		logrus.WithField("requestID", requestID).Error(err)
-		response.ClientErr(c, err.Error())
-		return
-	}
-
-	userState := auth.Perm.UserState()
-	if !userState.HasUser(param.Username) {
-		response.ClientErr(c, "user does not exist")
-		return
-	}
-	if !userState.CorrectPassword(param.Username, param.Password) {
-		response.ClientErr(c, "wrong password")
-		return
-	}
-	response.Success(c)
-}
