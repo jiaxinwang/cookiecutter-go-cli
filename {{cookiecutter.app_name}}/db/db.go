949ec7a29a0a55cc38d0c035fd988e7c7f964dd8
jiaxinwang <jiaxinwang@me.com>
2021-04-16 14:13:20 +0800

    ^ gorm

--- {{cookiecutter.app_name}}/db/db.go
+++ {{cookiecutter.app_name}}/db/db.go
@@ -1,20 +1,36 @@
 package db
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/sirupsen/logrus"
 
-	_ "github.com/jinzhu/gorm/dialects/{{cookiecutter.use_db}}"
+	"gorm.io/driver/{{cookiecutter.use_db}}"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
 )
 
 // DB ...
 var DB *gorm.DB
 
-// InitDB ...
+// Connect ...
 func Connect(dbURI string) {
 	var err error
 
-	if DB, err = gorm.Open("{{cookiecutter.use_db}}", dbURI); err != nil {
-		logrus.Panic(err)
+	var l logger.Interface
+	l = logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			// LogLevel:      logger.Info,
+			LogLevel: logger.Silent,
+			Colorful: true,
+		})
+
+	DB, err = gorm.Open({{cookiecutter.use_db}}.Open(dbURI), &gorm.Config{
+		Logger: l,
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
